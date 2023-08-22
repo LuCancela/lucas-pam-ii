d5b795aca716294f6beb72c0ad5d5194ec5b675d
LuCancela <cancelalucas@gmail.com>
2023-08-21 22:26:17 -0300

    Objetos em JS

--- ReactNative/IntrocucaoAoJs/Aula01-Variaveis.js
+++ ReactNative/IntrocucaoAoJs/Aula01-Variaveis.js
@@ -1,23 +1,24 @@
 //ampla mais útil podem se comportar no código inteiro fazendo alguma função
-let variavelLet = "Olá"
+let variavelLet = "Olá";
 
 //local pode ser que tenha que declarada várias vezes
-var variavelVar = 2
+var variavelVar = 2;
 
 //visizel em todo o código e também é imutavel, não é possivél alterar seu valor 
-const variavelConst = 3
+const variavelConst = 3;
 
 //imprimir o valor da váriavel
-console.log(variavelLet)
+console.log(variavelLet);
 // o + pode somar o valor das variáveis como também concatenar os valores (meio que juntar duas palavras)
-console.log(variavelVar + variavelConst)
+console.log(variavelVar + variavelConst);
 
 //mostra o tipo da variavel
-console.log(typeof variavelVar)
+console.log(typeof variavelVar);
 
-variavelLet = "Olá Mundo"
+variavelLet = "Olá Mundo";
 
 //melhor forma de concatenar(juntar ou acrescentar duas palavras)
-console.log(variavelLet, variavelConst)
+console.log(variavelLet, variavelConst);
+console.log(typeof variavelConst)
 
 
---
+++ ReactNative/IntrocucaoAoJs/Aula02.js
@@ -0,0 +1,26 @@
+
+
+
+const objPessoa = {
+    //declarando um objeto no JS
+
+    nome: "Lucas",// essa vírgula é para separar
+    signo: "Peixes",
+    idade: 18,
+    familia: ["Pai", "Mãe", "Irmãos"]
+
+}
+
+//Seleciona a linha Crtl + ; para selecionar uma linha
+
+// Formato JSON - Javascriot Notation Object toda a API retorna 1
+console.log(objPessoa.nome, objPessoa.signo);
+console.log(objPessoa.familia[1]);
+console.log(objPessoa);
+console.log(typeof objPessoa);
+
+
+//no cochetes[] o número que está dentro espressa qual dado colocar.
+let pessoa = ["Lucas", 18, "Aprediz de Desnvolvimento"];
+ console.log(pessoa[0], pessoa[2]);
+ console.log(typeof pessoa)
